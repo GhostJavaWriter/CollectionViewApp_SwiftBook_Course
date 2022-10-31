605f86bd9b5cc86aadf776670e1ee69531f051ef
Ghost <nadtsalov@gmail.con>
2022-10-31 16:11:22 +0300

    Delete unessesary code

--- Collection_1/View/PhotoViewCell.swift
+++ Collection_1/View/PhotoViewCell.swift
@@ -17,16 +17,6 @@ class PhotoViewCell: UICollectionViewCell {
         return view
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     func configureCell(with model: PetsModel) {
         let image = UIImage(named: model.photo)
         imageView.image = image

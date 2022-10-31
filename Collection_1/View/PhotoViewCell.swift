9c78e97e881616dd980e6dddf0e81b362633d433
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2022-10-31 18:56:36 +0300

    Update PhotoViewCell.swift

--- Collection_1/View/PhotoViewCell.swift
+++ Collection_1/View/PhotoViewCell.swift
@@ -22,8 +22,4 @@ class PhotoViewCell: UICollectionViewCell {
         imageView.image = image
         
     }
-    
-    func someFuncFromMainBranch() {
-        print("I'm from Main branch")
-    }
 }
